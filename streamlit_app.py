ca845198cb183d58a8f145b3a07eae8ec3aaea46
Amir <amirfathi.92@gmail.com>
2022-11-09 15:12:39 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
+# Structural interactome project, COSBI lab:
 
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
